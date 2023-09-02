532fd06f1402eede26878a3a3b69d76c37de7755
Burak <burakonturk2000@gmail.com>
2023-09-03 01:55:49 +0300

    .

--- Assets/Scripts/Controller/playerCam.cs
+++ Assets/Scripts/Controller/playerCam.cs
@@ -27,7 +27,7 @@ public class playerCam : MonoBehaviour
         yRotation += mouseX;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
